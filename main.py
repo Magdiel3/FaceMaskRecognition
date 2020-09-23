cc56bf68821877f9ff84bcf4b8ac858fe9944378
Magdiel3 <magdieltercero@hotmail.com>
2020-09-22 22:38:16 -0500

    Commented code

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 import cv2
 import os
 
+# Download the face recognition XML config file
 cascPath = os.path.dirname(
     cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
 
-print(cascPath)
+# Initialize algorithm
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Select the integrated webcam
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Detect the faces in the image
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # Draw a rectangle around every detected face
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0), 2)
+
+    # Display image
+    cv2.imshow('Video', frame)
+
+    # Define the quit key (q)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+video_capture.release()
+cv2.destroyAllWindows()
